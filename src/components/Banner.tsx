c4250e3519e9328d438996e25f35a49278ce988d
habib-71 <mdhabib18005@gmail.com>
2026-09-12 09:50:34 +0600

    add-banner-section

---
+++ src/components/Banner.tsx
@@ -0,0 +1,36 @@
+import BannerImg from "../assets/banner-stack.png";
+const Banner = () => {
+    return (
+        <section className="flex justify-between items-center container mx-auto min-h-screen">
+            <div className="w-1/2 max-w-xl">
+                <h1 className="text-6xl font-extrabold leading-tight">
+                    Build Your Ideal <br />
+                    <span className="bg-linear-to-r from-[#FF5722]  via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
+                        Development Stack
+                    </span>
+                </h1>
+                <p className="mt-5 max-w-xl text-gray-500 leading-7">
+                    Explore frontend, backend, database, and tooling options,
+                    <br />
+                    compare them side by side, and put together the stack that fits your
+                    next project.
+                </p>
+
+                <div className="flex gap-4 mt-12">
+                    <button className="px-6 py-3 rounded-xl text-white font-semibold bg-linear-to-r from-[#FF5722] to-[#D81B7E] shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300">
+                        Explore Technologies
+                    </button>
+                    <button className="px-14 py-3 rounded-xl border border-gray-300 font-semibold hover:bg-gray-100 hover:border-gray-400 hover:shadow-md hover:-translate-y-0.5 transition-all duration-300">
+                        Learn More
+                    </button>
+                </div>
+            </div>
+
+            <div className="w-1/2 flex justify-end">
+                <img src={BannerImg} alt="Banner Immage" />
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,40 @@
-import Logo from "../assets/logo-text.png"
+import Logo from "../assets/logo-text.png";
 const Nav = () => {
     return (
-       <div className="flex justify-between items-center container mx-auto mt-4  sticky">
-         <nav>
-            <img src={Logo} alt="Logo" />
-        </nav>
-        <ul className="flex justify-between gap-4 items-center">
-            <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">Home</li>
-            <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">Technologies</li>
-            <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">Projects</li>
-            <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">About</li>
-            <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">Contact</li>
-        </ul>
+   <nav className="border-b border-gray-300 shadow-sm sticky top-0 bg-white z-50 p-5">
+            <div className="flex justify-between items-center container mx-auto">
+                <div>
+                <img src={Logo} alt="Logo" />
+            </div>
+            <ul className="flex justify-between gap-4 items-center">
+                <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">
+                    Home
+                </li>
+                <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">
+                    Technologies
+                </li>
+                <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">
+                    Projects
+                </li>
+                <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">
+                    About
+                </li>
+                <li className=" hover:text-red-500 hover:underline cursor-pointer transition duration-300">
+                    Contact
+                </li>
+            </ul>
 
-        <ul className="flex gap-4 items-center">
-            <button className="px-5 py-2 font-medium text-black hover:text-red-500 transition duration-300">Sign In</button>
-            <button className="btn btn-secondary px-6 py-2 text-white font-medium rounded-full hover:shadow-lg transition duration-200">Sign Up</button>
-        </ul>
-       </div>
+            <ul className="flex gap-4 items-center">
+                <button className="px-5 py-2 font-medium text-black hover:text-red-500  transition duration-300">
+                    Sign In
+                </button>
+                <button className="btn btn-secondary px-6 py-2 text-white font-medium rounded-full shadow-md hover:shadow-xl hover:scale-105 transition duration-200">
+                    Sign Up
+                </button>
+            </ul>
+            </div>
+   </nav>
     );
 };
 
-export default Nav ;
+export default Nav;
