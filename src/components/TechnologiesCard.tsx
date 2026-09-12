9ee90e732c38edc3251b603c5c8c803d3f2d254e
habib-71 <mdhabib18005@gmail.com>
2026-09-13 00:28:13 +0600

    add-initial-stack-card

--- src/components/TechnologiesCard.tsx
+++ src/components/TechnologiesCard.tsx
@@ -5,51 +5,68 @@ interface TechnologiesCardProps {
 }
 const TechnologiesCard = ({ technologies }: TechnologiesCardProps) => {
     return (
-        <div className="grid grid-cols-3 gap-6 mt-6">
-            {technologies.map((technology: ITechnologies) => {
-                return (
+        <div className="flex gap-6 mt-6">
+            {/* technology cards */}
+            <div className="grid grid-cols-3 w-[70%] gap-6 mt-6">
+                {technologies.map((technology: ITechnologies) => {
+                    return (
 
-                    <div key={technology.id}className="border border-gray-200 rounded-2xl p-6 shadow-sm hover:-translate-y-2 hover:shadow-xl transition-all duration-300">
-                        <div className="flex justify-between items-start">
-                            <div className="w-12 h-12 flex items-center justify-center rounded-xl bg-gray-50 border border-gray-100">
-                                <img
-                                    className="w-8 h-8"
-                                    src={technology.icon}
-                                    alt={technology.name}
-                                />
+                        <div key={technology.id} className="border border-gray-200 rounded-2xl p-6 shadow-sm hover:-translate-y-2 hover:shadow-xl transition-all duration-300">
+                            <div className="flex justify-between items-start">
+                                <div className="w-12 h-12 flex items-center justify-center rounded-xl bg-gray-50 border border-gray-100">
+                                    <img
+                                        className="w-8 h-8"
+                                        src={technology.icon}
+                                        alt={technology.name}
+                                    />
+                                </div>
+
+                                <span className="px-3 py-1 rounded-full text-sm font-medium bg-pink-50 text-pink-600 border border-pink-100">
+                                    {technology.badge}
+                                </span>
                             </div>
 
-                            <span className="px-3 py-1 rounded-full text-sm font-medium bg-pink-50 text-pink-600 border border-pink-100">
-                                {technology.badge}
-                            </span>
+                            <h2 className="mt-6 text-2xl font-bold text-gray-900">
+                                {technology.name}
+                            </h2>
+
+                            <p className="mt-3 text-sm leading-6 text-gray-500">
+                                {technology.description}
+                            </p>
+
+                            <div className="flex flex-wrap justify-between gap-2 mt-5">
+                                <span className="px-3 py-1 rounded-full bg-orange-50 text-orange-600 text-xs font-medium border border-orange-100">
+                                    {technology.category}
+                                </span>
+
+                                <span className="px-3 py-1 rounded-full bg-purple-50 text-purple-600 text-xs font-medium border border-purple-100">
+                                    {technology.difficulty}
+                                </span>
+                                <span className="px-3 py-1 rounded-full bg-yellow-50 text-yellow-600 text-xs font-medium border border-yellow-100">
+                                    ⭐ {technology.rating}
+                                </span>
+                            </div>
+                            <button className="w-full mt-5 px-4 py-3 rounded-xl text-sm font-semibold text-white bg-linear-to-r from-[#FF5722] to-[#D81B7E] shadow-sm hover:shadow-md hover:scale-[1.02] transition-all duration-300">
+                                Add to Stack
+                            </button>
                         </div>
-
-                        <h2 className="mt-6 text-2xl font-bold text-gray-900">
-                            {technology.name}
-                        </h2>
-
-                        <p className="mt-3 text-sm leading-6 text-gray-500">
-                            {technology.description}
-                        </p>
-
-                        <div className="flex flex-wrap justify-between gap-2 mt-5">
-                            <span className="px-3 py-1 rounded-full bg-orange-50 text-orange-600 text-xs font-medium border border-orange-100">
-                                {technology.category}
-                            </span>
-
-                            <span className="px-3 py-1 rounded-full bg-purple-50 text-purple-600 text-xs font-medium border border-purple-100">
-                                {technology.difficulty}
-                            </span>
-                            <span className="px-3 py-1 rounded-full bg-yellow-50 text-yellow-600 text-xs font-medium border border-yellow-100">
-                                ⭐ {technology.rating}
-                            </span>
-                        </div>
-                        <button className="w-full mt-5 px-4 py-3 rounded-xl text-sm font-semibold text-white bg-linear-to-r from-[#FF5722] to-[#D81B7E] shadow-sm hover:shadow-md hover:scale-[1.02] transition-all duration-300">
-                            Add to Stack
-                        </button>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
+            {/* Stack Card */}
+            <div className="w-[30%] self-start border border-gray-200 rounded-2xl p-5 shadow-sm mt-6">
+                <h2 className="text-xl font-bold text-gray-900">
+                    Your Stack
+                </h2>
+                <p className="mt-2 text-sm text-gray-500">
+                    No technology selected
+                </p>
+                <div className="mt-5 border border-dashed border-gray-300 rounded-xl p-8 text-center">
+                    <h3 className="text-sm font-semibold text-gray-700">
+                        Your stack is empty
+                    </h3>
+                </div>
+            </div>
         </div>
     );
 };
